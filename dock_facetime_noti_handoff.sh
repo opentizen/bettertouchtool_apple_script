32e4f7fb60be6d738fa66290028966af16f59331
kissd3v <kissd3v@gmail.com>
2019-01-13 02:53:20 +0900

    add dock_facetime

---
+++ dock_facetime_noti_handoff.sh
@@ -0,0 +1,24 @@
+tell application "System Events"
+	tell process "Dock"
+		try
+			set badgeNumber to value of attribute "AXStatusLabel" of UI element "FaceTime" of list 1
+			if badgeNumber exists then
+				if badgeNumber contains "iPhone" then
+					return "iPhone에서"
+				else if badgeNumber contains "iPad" then
+					return "iPad에서"
+				else if badgeNumber contains "watch" then
+					return "Watch에서"
+				else if badgeNumber contains "Mac" then
+					return "Mac에서"
+				else
+					return badgeNumber
+				end if
+			else
+				return ""
+			end if
+		on error
+			return ""
+		end try
+	end tell
+end tell
